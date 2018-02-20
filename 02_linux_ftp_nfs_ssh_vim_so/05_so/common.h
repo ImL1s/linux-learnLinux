8396d1cc1a015209939ca48040157c755a5fa356
ImL1s <aa22396584@gmail.com>
2018-02-20 20:12:31 +0800

    first commit

---
+++ 02_linux_ftp_nfs_ssh_vim_so/05_so/common.h
@@ -0,0 +1,7 @@
+#ifndef COMMON_FILE_H
+# define COMMON_FILE_H
+int plus(int a, int b);
+int div(int,int);
+int sub(int,int);
+int mul(int,int);
+#endif
---
+++ 02_linux_ftp_nfs_ssh_vim_so/05_so/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "common.h"
+
+int main(void)
+{
+    printf("plus: %d\n",plus(1,2));
+    printf("sub: %d\n",sub(1,2));
+    printf("mul: %d\n",mul(1,2));
+    printf("div: %d\n",div(1,2));
+    return 0;
+}
